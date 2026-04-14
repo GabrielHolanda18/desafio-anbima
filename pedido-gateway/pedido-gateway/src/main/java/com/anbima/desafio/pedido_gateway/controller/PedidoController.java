a59ce3ff0d80849f4622e969a22e13b561576cec
Gabriel <gabrielholanda18@hotmail.com>
2026-04-14 20:25:57 -0300

    feat: adiciona lógica de negócio do gateway (service e controller)

--- pedido-gateway/pedido-gateway/src/main/java/com/anbima/desafio/pedido_gateway/controller/PedidoController.java
+++ pedido-gateway/pedido-gateway/src/main/java/com/anbima/desafio/pedido_gateway/controller/PedidoController.java
@@ -1,7 +1,11 @@
 package com.anbima.desafio.pedido_gateway.controller;
 
+import com.anbima.desafio.pedido_gateway.entity.Pedido;
 import com.anbima.desafio.pedido_gateway.service.PedidoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,12 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(name = "/pedidos")
+@RequiredArgsConstructor
 public class PedidoController {
 
-
+    private final PedidoService pedidoService;
+    /*
     @PostMapping(value = "/posicional", consumes = MediaType.TEXT_PLAIN_VALUE)
-    public ResponseEntity<PedidoService> recebePedido(@RequestBody String linha){
+    public ResponseEntity<Pedido> recebePedido(@RequestBody String linha){
 
-        //Validações
+        // Chamamos o service e guardamos o pedido que ele salvou e calculou
+        Pedido pedidoSalvo = pedidoService.tratamentoEntrada(linha);
+
+        // Retornamos o status 201 (Created) e o pedido no corpo da resposta
+        return ResponseEntity.status(HttpStatus.CREATED).body(pedidoSalvo);
+
+
+        pedidoService.tratamentoEntrada(linha);
+        return RequestEntity.ok().Build();
+
+
     }
+    */
 }
--- pedido-gateway/pedido-gateway/src/main/java/com/anbima/desafio/pedido_gateway/service/PedidoService.java
+++ pedido-gateway/pedido-gateway/src/main/java/com/anbima/desafio/pedido_gateway/service/PedidoService.java
@@ -1,8 +1,47 @@
 package com.anbima.desafio.pedido_gateway.service;
 
+import com.anbima.desafio.pedido_gateway.entity.Pedido;
+import com.anbima.desafio.pedido_gateway.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PedidoService {
 
-    public PedidoService(String linha){
+    private PedidoRepository repository;
 
+    // Troca void por Pedido
+    public void tratamentoEntrada(String linha) {
+
+        if (linha == null || linha.length() != 40) {
+            throw new RuntimeException("A entrada deve ter exatamente 40 caracteres.");
+        }
+
+        // Repartindo a ‘String’ recebida para realizar a montagem do Pedido
+
+        String tipoLache = linha.substring(0,10).trim(); // 1 a 10
+        String proteina = linha.substring(10,20).trim(); // 11 a 20
+        String acompanhamento =  linha.substring(20,30).trim(); // 21 a 30
+
+        // Verificar se a quantidade veio em forma de numérica
+
+        String quantidadeStr = linha.substring(30,32).trim(); // 31 a 32
+        if (!quantidadeStr.matches("\\d{2}")){
+            throw new RuntimeException("Entrada deve ser numérica (01 até 99)");
+        }
+        // Transformar quantidade em Inteiro
+        Integer quantidade = Integer.parseInt(quantidadeStr);
+
+        String bebida =  linha.substring(32,40).trim(); // 33 a 40
+
+        // Transformar a 'String' em um Objeto do tipo Pedido
+
+        //Pedido pedido = Pedido.builder().
+
+    }
+
+    private boolean calcularPreco(Pedido pedido){
+
+        return true;
     }
 }
